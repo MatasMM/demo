ae8c874a1cbcd4e81824de4104bb8fe63b7e3da5
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-01-02 11:37:58 +0200

    String number

--- metodai/number.js
+++ metodai/number.js
@@ -35,3 +35,22 @@ console.log(parseInt(7.5));
 console.log(parseInt('5'));
 console.log(parseInt(Infinity));
 console.log(parseInt('labas'));
+
+const a = 3.1415;
+const b = a.toFixed(2);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+
+const c = parseInt(b) + parseInt(b);
+console.log(c);
+
+const d = parseFloat(b) + parseFloat(b);
+console.log(d);
+
+console.log(isNaN(3));
+console.log(isNaN(NaN));
+
+console.log(isFinite(NaN));
+console.log(isFinite(Infinity));
+console.log(isFinite(14562));
