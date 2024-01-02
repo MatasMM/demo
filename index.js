0268a07fbd0c662aa317342427c111569d78679c
MatasMM <153711311+MatasMM@users.noreply.github.com>
2024-01-02 08:49:05 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Labas rytas, Lietuva');
+//console.log('antra eilute');
+//console.log('trecia eilute');
+//console.log('454845112');
+
+/*
+BAZINEI PROGRAMAVIMO KONCEPAI
+- duomenu tipai
+    - number
+    - string
+    - boolean
+    - array
+    - undefined
+- if
+- for
+- function
+- events
+*/
